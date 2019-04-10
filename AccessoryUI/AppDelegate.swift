be6eca41c5a156ae85d486d691100192ec48e03d
Apple <>
2019-04-10 13:23:29 -0800

    Initial commit.

---
+++ AccessoryUI/AppDelegate.swift
@@ -0,0 +1,16 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The app delegate.
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+
+    var window: UIWindow?
+
+}
+
